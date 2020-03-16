36a3892f8a304ecf8fd7fba5eb611424f9e3510a
Ilya <gusarov2000@gmail.com>
2020-03-16 21:28:33 +0300

    fixed sign_up and sign_in logic added user events

--- src/main/java/com/calendar/controller/UserRestController.java
+++ src/main/java/com/calendar/controller/UserRestController.java
@@ -1,12 +1,18 @@
 package com.calendar.controller;
 
+import com.calendar.entity.Event;
 import com.calendar.entity.User;
+import com.calendar.exception.EventNotFoundException;
+import com.calendar.exception.UserAlreadyRegistered;
 import com.calendar.exception.UserNotFoundException;
 import com.calendar.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @RestController
 public class UserRestController {
@@ -14,29 +20,42 @@ public class UserRestController {
     @Autowired
     UserRepository repository;
 
-    @PostMapping("/users/sign_up")
+    @PostMapping("/api/users/sign_up")
     public User signUp(@RequestBody User newUser) {
+        String mail = newUser.getEmail();
+        repository.findByEmail(mail).ifPresent(user -> {
+            throw new UserAlreadyRegistered(user.getEmail());
+        });
+
         return repository.save(newUser);
     }
 
-    @GetMapping("/users/sign_in")
-    public User signIn(@RequestParam String email, @RequestParam String pass) {
-        return repository.findByEmailAndAndPass(email, pass)
-                .orElseThrow(() -> new UserNotFoundException(email));
+    @PostMapping("/api/users/sign_in")
+    public User signIn(@RequestBody User user) {
+        String mail = user.getEmail();
+        return repository.findByEmail(mail)
+                .map(user1 -> {
+                        if (user1.getPass().equals(user.getPass())) {
+                            return user1;
+                        } else {
+                          throw new UserNotFoundException(mail);
+                        }
+                })
+                .orElseThrow(UserNotFoundException::new);
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/api/users/{id}")
     public User getUser(@PathVariable Long id) {
         return repository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
 
-    @GetMapping("/users")
+    @GetMapping("/api/users")
     public List<User> getAll() {
         return repository.findAll();
     }
 
-    @PutMapping("users/update/{id}")
+    @PutMapping("/api/users/update/{id}")
     public User update(@PathVariable Long id, @RequestParam String pass) {
 
         return repository.findById(id)
@@ -44,6 +63,13 @@ public class UserRestController {
                     employee.setPass(pass);
                     return repository.save(employee);
                 })
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @GetMapping("/api/users/{id}/events")
+    public Set<Event> getEventUsers(@PathVariable Long id) {
+        return repository.findById(id)
+                .map(User::getEvents)
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
 }
